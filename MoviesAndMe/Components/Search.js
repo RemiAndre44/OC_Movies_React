927dce0a16459e910cc1c0a8bd31592c06f1db4a
SantiKush <remi44andre@gmail.com>
2019-06-12 18:03:34 +0200

    Liste début

--- MoviesAndMe/Components/Search.js
+++ MoviesAndMe/Components/Search.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { StyleSheet, View, Button, TextInput, FlatList, Text } from 'react-native'
 
-class Search extends React.component{
+class Search extends React.Component{
   render(){
     return(
-
+      <View style={ styles.mainContainer }>
+        <TextInput placeholder="Titre du film" style={styles.textInput }/>
+        <Button style={{ height: 50 }} title="Rechercher" onPress={() => {}}/>
+        <FlatList
+          data={[{key: 'a'}, {key: 'b'}]}
+          renderItem={({item}) => <Text>{item.key}</Text>}
+        />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  mainContainer:{
+    marginTop: 25,
+    flex: 1
+  },
+  textInput: {
+    marginLeft: 5,
+    marginRight:5,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
+
+export default Search
